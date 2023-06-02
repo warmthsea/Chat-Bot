ef7ec2cf394c94fcb39e56aedf2f75438194e183
huayang wang <2586244885@qq.com>
2023-06-02 16:21:29 +0800

    update logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="h-12 font-semibold flex items-center justify-center">
-    <img src="/logo.svg" class="w-6 h-6 animate-pulse">
+    <!-- <img src="/logo.svg" class="w-6 h-6 animate-pulse"> -->
     <span class="mt-1 ml-2 mr-4 font-serif">Chat Bot</span>
   </div>
 </template>
